fe78a1060c41dc10d50861c2afa1bc6cd6722155
weput <weput@users.noreply.github.com>
2017-07-26 10:53:31 -0500

    Update shoppingcart-2checkout-dev.php
    removed unused code lines

--- shoppingcart-2checkout-dev.php
+++ shoppingcart-2checkout-dev.php
@@ -26,7 +26,6 @@ class Shoppingcart2checkoutDevPlugin extends Plugin
      */
     public function onTwigSiteVariables()
     {
-        $this->grav['assets']->addJs('https://www.2checkout.com/static/checkout/javascript/direct.min.js');
         $this->grav['assets']->addJs('plugin://' . $this->plugin_name . '/gateway/2checkout/script.js');
 
 
@@ -68,31 +67,4 @@ class Shoppingcart2checkoutDevPlugin extends Plugin
         require_once($path);
     }
     
-        public function getGateway()
-    {
-        if (!$this->gateway) {
-            $this->requireGateway();
-            require_once __DIR__ . '/gateways/2checkout/gateway.php';
-            $this->gateway = new ShoppingCart\Gateway2checkout();
-        }
-
-        return $this->gateway;
-    }
-    /**
-     * Do some work for this event, full details of events can be found
-     * on the learn site: http://learn.getgrav.org/plugins/event-hooks
-     *
-     * @param Event $e
-     */
-//    public function onPageContentRaw(Event $e)
-//    {
-//        // Get a variable from the plugin configuration
-//        $text = $this->grav['config']->get('plugins.shoppingcart-2checkout-dev.payment.methods.twocheckout.description');
-//
-//        // Get the current raw content
-//        $content = $e['page']->getRawContent();
-//
-//        // Prepend the output with the custom text and set back on the page
-//        $e['page']->setRawContent($text . "\n\n" . $content);
-//    }
 }
